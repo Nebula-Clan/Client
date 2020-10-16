c4882f3c4e03a708c20f150ac2fb5c95216d5a92
Amin-MAG <M.Amin.Ghasvari@gmail.com>
2020-10-16 19:29:05 +0330

    add color

--- components/login/SignIn.vue
+++ components/login/SignIn.vue
@@ -7,12 +7,15 @@
     <v-divider class="my-6"></v-divider>
     <v-form>
       <v-text-field
+        color="blue lighten-2"
         label="Username"/>
       <v-text-field
+        color="blue lighten-2"
         label="Password"
         type="password"/>
       <v-btn
         class="mt-5"
+        color="green darken-1"
         block
         depressed>
         Sign In
@@ -20,10 +23,16 @@
     </v-form>
     <v-divider class="my-6"></v-divider>
     <div>
-      <v-btn class="mt-3 pa-5" block>
+      <v-btn
+        class="mt-3 pa-5"
+        color="red darken-1"
+        block>
         <i class="fab fa-google mr-3"/> Sign in with Google
       </v-btn>
-      <v-btn class="mt-3 pa-5" block>
+      <v-btn
+        class="mt-3 pa-5"
+        color="blue darken-1"
+        block>
         <i class="fab fa-facebook-f mr-3"/> Sign in with Facebook
       </v-btn>
     </div>
