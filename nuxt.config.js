bbe41f5659f2cf17b1416b0aa710c0650720c7b5
Amin-MAG <M.Amin.Ghasvari@gmail.com>
2020-10-16 16:17:32 +0330

    add fontawesome5

--- nuxt.config.js
+++ nuxt.config.js
@@ -17,7 +17,8 @@ export default {
       { hid: 'description', name: 'description', content: '' }
     ],
     link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
+      { rel: 'stylesheet', href: 'https://use.fontawesome.com/releases/v5.0.13/css/all.css' }
     ]
   },
 
