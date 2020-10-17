9a66f42d4a9e8d266560010d156c4b8ac2a5ec51
Amir Esmaeili <amir-esmaeili@outlook.com>
2020-10-17 23:00:57 +0330

    banner - psw visibility

--- pages/login.vue
+++ pages/login.vue
@@ -7,10 +7,16 @@
           class="d-flex justify-center"
           cols="12">
           <SignIn/>
-          <img
-            class="elevation-20 intro-img"
-            src="../static/images/unnamed.jpg"
-            alt="">
+          <div class="intro d-flex justify-center">
+            <div class="intro-text pa-5 rounded-lg text-center">
+              <h1>Huddle Is Your Universe!</h1>
+
+              <p class="mt-10 font-weight-black">Write what comes to your mind ...</p>
+              <p class="font-weight-bold">Explore other universes...</p>
+              <p class="font-weight-medium">And more fun stuff...</p>
+
+            </div>
+          </div>
         </v-col>
       </v-row>
   </div>
@@ -30,8 +36,20 @@ export default {
   background: url("../static/images/login-background.jpg") no-repeat;
   background-size: cover;
   height: 100%;
-  .intro-img {
+  .intro {
     border-radius: 0 10px 10px 0;
+    background-image: url("../static/images/universe.jpg");
+    background-size: cover;
+    min-width: 80vh;
+    .intro-text {
+      color: black;
+      width: 80%;
+      margin: auto;
+      background-color: rgba(255, 255, 255, 0.6);
+      h1, p{
+        color: #2e0057;
+      }
+    }
   }
 }
 </style>
