995e1038c5d3afd5b6de33da1b4c7a77e1cf33f5
keyvan1379 <ddssee71@gmail.com>
2020-10-21 14:55:00 -0700

    profileintro updated profile posts added

---
+++ components/profile/ProfileComments.vue
@@ -0,0 +1,63 @@
+<template>
+    <v-container fluid>
+        <v-row>
+            <v-card max-width="740">
+                <v-list-item three-line>
+                     <v-list-item-avatar tile size="80" color="grey">
+                        <v-list-item-icon>
+                            <v-icon >mdi-account</v-icon>
+                        </v-list-item-icon>
+                    </v-list-item-avatar>
+                    <v-list-item-content>
+                        <div class="overline mb-3">
+                        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ducimus sint reprehenderit sapiente quisquam ad. Iusto officiis vitae assumenda soluta vel?
+                        </div>
+                        <v-list-item-subtitle>
+                            <v-row class="ml-1">
+                                <p v-for="hashtag in hashtags" :key="hashtag" class="mx-1 blue--text text--lighten-1">
+                                    {{ hashtag }}
+                                </p>
+                            </v-row>
+                            <v-row>
+                                <v-list-item-icon class="ml-3">
+                                    <v-icon>mdi-flag</v-icon>
+                                </v-list-item-icon>
+                                <v-list-item-icon class="ml-3">
+                                    <v-icon>mdi-like</v-icon>
+                                </v-list-item-icon>
+                                <v-list-item-icon class="ml-3">
+                                    <v-icon>mdi-like</v-icon>
+                                </v-list-item-icon>
+                            </v-row>
+                        </v-list-item-subtitle>
+                    </v-list-item-content>
+                </v-list-item>
+            </v-card>
+        </v-row>
+    </v-container>
+</template>
+
+
+<script>
+export default {
+    data: () => {
+        return {
+            hashtags: [
+                "#idk",
+                "#idc",
+                "#hallo",
+                "#test",
+                "#foo",
+                "#bar",
+                "#barbar",
+                "#bitch",
+                '#goaway'
+            ]
+        }
+    }
+}
+</script>
+
+<style scoped>
+
+</style>
--- components/profile/ProfileIntro.vue
+++ components/profile/ProfileIntro.vue
@@ -5,7 +5,16 @@
             <v-col cols="3">
                 <ProfileDescription />
             </v-col>
-
+            <v-col cols="6">
+                <v-tabs style="border-radius:2px">
+                    <v-tab>Posts</v-tab>
+                    <v-tab>Comments</v-tab>
+                    <v-tab>Likes</v-tab>
+                </v-tabs>
+                <v-container class="ml-0" v-for="i in n" :key="i">
+                    <ProfileComments />
+                </v-container>
+            </v-col>
         </v-row>
     </v-container>
 </template>
@@ -13,11 +22,12 @@
 <script>
 import ProfileBanner from './ProfileBanner'
 import ProfileDescription from './ProfileDescription'
+import ProfileComments from './ProfileComments'
 
 export default {
     data: () => {
         return {
-
+            n:10
         }
     }
 }
