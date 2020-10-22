ff1380c139bcfa0b2d1e0b1d8fa65635bc4886de
Amir Esmaeili <amir-esmaeili@outlook.com>
2020-10-22 14:44:28 +0330

    profile right side

---
+++ components/homepage/user.component.vue
@@ -0,0 +1,81 @@
+<template>
+  <v-card >
+    <div class="profile mb-10">
+      <v-img
+            class="white--text align-end custom-rounded"
+            src="https://cdn.vuetifyjs.com/images/cards/docks.jpg"
+            height="80px">
+      </v-img>
+      <v-avatar
+        class="profile-pic"
+        size="80">
+        <img
+          src="https://cdn.vuetifyjs.com/images/john.jpg"
+          alt="John">
+      </v-avatar>
+    </div>
+    <v-card-title  class="justify-center">
+      {{firstname}} {{lastname}}
+    </v-card-title>
+    <v-card-subtitle class="text-center">
+      {{username}}
+    </v-card-subtitle>
+    <v-divider></v-divider>
+    <v-row class="text-center pa-2 justify-space-around">
+      <div>
+        <b>{{followings}}</b>
+        <br>
+        <span>Followings</span>
+      </div>
+      <v-divider
+        vertical
+      ></v-divider>
+      <div>
+        <b>{{posts}}</b>
+        <br>
+        <span>Posts</span>
+      </div>
+      <v-divider
+        vertical
+      ></v-divider>
+      <div>
+        <b>{{followers}}</b>
+        <br>
+        <span>Followers</span>
+      </div>
+    </v-row>
+  </v-card>
+</template>
+
+<script>
+export default {
+  name: "user.component",
+  data: () => {
+    return {
+      username: 'amiresm',
+      firstname: 'Amir',
+      lastname: 'Esmaeili',
+      followers: 100,
+      followings: 343,
+      posts: 1000
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.profile {
+  position: relative;
+  .profile-pic {
+    position: absolute;
+    bottom: -40px;
+    left: calc(50% - 40px);
+  }
+  .custom-rounded {
+    border-radius: 5px 5px 0 0;
+  }
+}
+b, span {
+  font-size: x-small;
+}
+</style>
---
+++ pages/feed.vue
@@ -0,0 +1,27 @@
+<template>
+  <v-row class="pa-5">
+    <v-col cols="3">
+    </v-col>
+    <v-col cols="6">
+      <p>Main</p>
+    </v-col>
+    <v-col cols="3">
+      <User />
+    </v-col>
+  </v-row>
+</template>
+
+<script>
+import User from '~/components/homepage/user.component'
+
+export default {
+  name: 'feed',
+  components: {
+    User
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
