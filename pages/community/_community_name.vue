f8fb97535461273d06970c8262d42969bc61cbf5
Amin-MAG <M.Amin.Ghasvari@gmail.com>
2020-11-19 18:19:33 +0330

    add loader to community

--- pages/community/_community_name.vue
+++ pages/community/_community_name.vue
@@ -1,88 +1,123 @@
 <template>
   <div>
-    <Intro
-      @userState="getInfo"
-      v-if="introData"
-      :data="introData"
-      :userJoined="isUserJoined"/>
+    <!--    <Intro-->
+    <!--      @userState="getInfo"-->
+    <!--      v-if="introData"-->
+    <!--      :data="introData"-->
+    <!--      :userJoined="isUserJoined"/>-->
+    <HeaderLoader v-show="true"/>
+
     <v-row class="pa-5">
       <v-col cols="3">
-        <About v-if="introData" :name="introData.name" :about="introData.about"/>
+        <!--        <About v-if="introData" :name="introData.name" :about="introData.about"/>-->
+        <v-card v-show="true" class="pa-2">
+          <div class="ma-2">
+            <v-skeleton-loader
+              type="heading"/>
+          </div>
+          <div
+            class="ma-2">
+            <v-skeleton-loader
+              type="paragraph, paragraph, paragraph, paragraph"/>
+          </div>
+        </v-card>
       </v-col>
       <v-col>
-        <Write v-if="name" :community="name" @posted="getPosts" />
-        <br>
-        <PostQuickView v-if="posts" :key="post.id" v-for="post in posts" :post="post"/>
+        <!--        <Write v-if="name" :community="name" @posted="getPosts"/>-->
+        <WriteLoader/>
+
+        <!--        <PostQuickView v-if="posts" :key="post.id" v-for="post in posts" :post="post"/>-->
+        <PostQuickViewLoader v-show="true" v-for="i in 5"/>
       </v-col>
       <v-col cols="3">
-        <People v-if="members" :people="members" />
+        <!--        <People v-if="members" :people="members" />-->
+        <v-card v-show="true" class="pa-2">
+          <div class="ma-2">
+            <v-skeleton-loader
+              type="heading"/>
+          </div>
+          <div
+            v-for="i in 10"
+            class="ma-2">
+            <v-skeleton-loader
+              type="list-item-avatar"/>
+          </div>
+        </v-card>
       </v-col>
     </v-row>
   </div>
 </template>
 
 <script>
-import Intro from "@/components/community/Header";
-import People from "@/components/community/People";
-import PostQuickView from '@/components/homepage/Post-quick-view'
-import About from "@/components/community/About";
-import Write from "@/components/homepage/Write";
-export default {
-  name: "communityName",
-  components: {
-    Write,
-    About,
-    People,
-    Intro,
-    PostQuickView
-  },
-  data() {
-    return {
-      introData: null,
-      isUserJoined: false,
-      members: null,
-      posts: null,
-      name: null
-    }
-  },
-  mounted() {
-    this.getInfo();
-    this.getPosts();
-  },
-  methods: {
-    getInfo() {
-      this.name = this.$route.params.community_name;
-      this.$axios.get(`api/community/get_community?name=${this.name}&summery=f`)
-      .then(
-        response => {
-          this.introData = {
-            id: response.data.community.id,
-            name: response.data.community.name,
-            logo: response.data.community.picture,
-            banner: response.data.community.banner_picture,
-            members: response.data.community.members_number,
-            since: response.data.community.date_created,
-            about: response.data.community.about
-          };
-          this.members = response.data.community.members;
-          const user = this.members.filter(u => u.username === this.$auth.user.username);
-          this.isUserJoined = user.length === 1;
-          // this.posts = response.data.community.posts
-        }
-      ).catch(
-        e => this.$notifier.showMessage({content: e.message, color: 'error'})
-      );
+  import Intro from "@/components/community/Header";
+  import People from "@/components/community/People";
+  import PostQuickView from '@/components/homepage/Post-quick-view'
+  import About from "@/components/community/About";
+  import Write from "@/components/homepage/Write";
+  import WriteLoader from "../../components/homepage/Write-Loader";
+  import HeaderLoader from "../../components/community/Header-Loader";
+  import PostQuickViewLoader from "../../components/homepage/Post-Quick-View-Loader";
+
+  export default {
+    name: "communityName",
+    components: {
+      PostQuickViewLoader,
+      HeaderLoader,
+      WriteLoader,
+      Write,
+      About,
+      People,
+      Intro,
+      PostQuickView
     },
-    getPosts() {
-      this.$axios.get(`api/community/community_posts?name=${this.name}`).then(
-        response => this.posts = response.data.posts
-      ).catch(
-        e => this.$notifier.showMessage({content: e.message, color: 'error'})
-      );
+    data() {
+      return {
+        introData: null,
+        isUserJoined: false,
+        members: null,
+        posts: null,
+        name: null
+      }
+    },
+    mounted() {
+      this.getInfo();
+      this.getPosts();
+    },
+    methods: {
+      getInfo() {
+        this.name = this.$route.params.community_name;
+        this.$axios.get(`api/community/get_community?name=${this.name}&summery=f`)
+          .then(
+            response => {
+              this.introData = {
+                id: response.data.community.id,
+                name: response.data.community.name,
+                logo: response.data.community.picture,
+                banner: response.data.community.banner_picture,
+                members: response.data.community.members_number,
+                since: response.data.community.date_created,
+                about: response.data.community.about
+              };
+              this.members = response.data.community.members;
+              const user = this.members.filter(u => u.username === this.$auth.user.username);
+              this.isUserJoined = user.length === 1;
+              // this.posts = response.data.community.posts
+            }
+          ).catch(
+          e => this.$notifier.showMessage({content: e.message, color: 'error'})
+        );
+      },
+      getPosts() {
+        this.$axios.get(`api/community/community_posts?name=${this.name}`).then(
+          response => this.posts = response.data.posts
+        ).catch(
+          e => this.$notifier.showMessage({content: e.message, color: 'error'})
+        );
+      }
     }
   }
-}
 </script>
 
 <style lang="scss" scoped>
+
 </style>
