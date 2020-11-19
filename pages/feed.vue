af6cd2415ef8a2c5214369981f30c90094718bd8
Amin-MAG <M.Amin.Ghasvari@gmail.com>
2020-11-19 07:51:26 +0330

    add loader to home page (communities & posts)

--- pages/feed.vue
+++ pages/feed.vue
@@ -15,7 +15,14 @@
       <Write @posted="getPosts"/>
       <br>
       <PostQuickView
+        v-show="!isPostLoading"
         v-for="(post, i) in posts" :key="i" :post="post"/>
+      <v-skeleton-loader
+        v-show="isPostLoading"
+        v-for="i in 5"
+        class="mb-2 pa-1"
+        elevation="2"
+        type="card-heading, card, list-item-three-line, list-item-avatar"/>
     </v-col>
     <v-col
       cols="12"
@@ -24,7 +31,19 @@
       sm="3">
       <User :user="this.$auth.user"/>
       <br>
-      <Communities v-if="communities" :communities="communities" @refresh="refresh"/>
+      <Communities
+        v-show="!isCommunitiesLoading"
+        :communities="communities"
+        @refresh="refresh"/>
+      <v-skeleton-loader
+        v-show="isCommunitiesLoading"
+        elevation="2"
+        type="card-heading"/>
+      <v-skeleton-loader
+        v-show="isCommunitiesLoading"
+        v-for="i in 5"
+        elevation="2"
+        type="list-item-avatar"/>
     </v-col>
   </v-row>
 </template>
@@ -48,22 +67,28 @@ export default {
   data() {
     return {
       posts: null,
-      communities: []
+      communities: [],
+
+      isPostLoading: true,
+      isCommunitiesLoading: true
     }
   },
   methods: {
     getPosts() {
+      // Posts
       this.$axios.get('api/posts/home_posts?order_key=new')
         .then((res) => {
-          this.posts = res.data.posts
-        }).catch(
-          e => this.$notifier.showMessage({content: e.message, color: 'error'})
-      );
+          this.posts = res.data.posts;
+          this.isPostLoading = false;
+        })
+        .catch(e => this.$notifier.showMessage({content: e.message, color: 'error'}));
+      // Communities
       this.$axios.get('api/community/user_communities').then(
-        response => this.communities = response.data.communities
-      ).catch(
-        e => this.$notifier.showMessage({content: e.message, color: 'error'})
-      );
+        ({data}) => {
+          this.communities = data.communities;
+          this.isCommunitiesLoading = false;
+        }
+      ).catch(e => this.$notifier.showMessage({content: e.message, color: 'error'}));
     },
     refresh() {
       console.log('refresh');
