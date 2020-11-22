1bad6871e904ffa43df7a96bd6b684598eb85cf0
keyvan1379 <ddssee71@gmail.com>
2020-11-22 15:15:46 -0800

    user list in chats added

---
+++ components/chat/Chat.vue
@@ -0,0 +1,22 @@
+<template>
+    <v-row justify="start" align="center" no-gutters>
+        <v-col cols="12" xl="3" lg="3">
+            <UserLists />
+        </v-col>
+        <v-col cols="12" xl="9" lg="9">
+            
+        </v-col>
+    </v-row>
+</template>
+
+
+<script>
+import UserLists from '~/components/chat/UserLists'
+export default {
+    
+}
+</script>
+
+<style scoped>
+
+</style>
