eab0cae0f9559ef26a78a04a7aff14dc2c3094c9
Amin-MAG <M.Amin.Ghasvari@gmail.com>
2020-11-27 19:53:23 +0330

    remove profile info component

--- pages/profile/_username.vue
+++ pages/profile/_username.vue
@@ -1,18 +1,178 @@
 <template>
-    <v-app>
-        <v-container fluid>
-            <v-row justify="start" align="center">
-                <ProfileIntro />
-            </v-row>
+  <v-container fluid class="pa-0">
+    <ProfileBanner/>
+    <v-row>
+      <v-col cols="12" lg="3" sm="12">
+        <ProfileDescription/>
+      </v-col>
+      <v-col cols="12" lg="6" sm="12">
+        <v-tabs style="border-radius:2px" class="tab-stick mb-4" v-model="selectedTab">
+          <v-tab @click="switchToPosts" :key="1">Posts</v-tab>
+          <v-tab @click="switchToComments" :key="2">Comments</v-tab>
+          <v-tab @click="switchToLikes" :key="3">Likes</v-tab>
+        </v-tabs>
+        <v-container class="ml-0 pa-0" v-for="(object, index) in getComponentObjects" :key="index">
+          <component :is="comp" v-bind="componentArgs(index)" @hook:mounted="countChild"></component>
         </v-container>
-    </v-app>
+      </v-col>
+      <v-col cols="12" lg="3" sm="12">
+        <ProfileStatusChart :height="200" :commentCount="20" :likeCount="30" :postCount="10"/>
+      </v-col>
+    </v-row>
+  </v-container>
 </template>
 
 <script>
-import ProfileIntro from '~/components/profile/ProfileIntro'
-export default {
+  import {mapGetters, mapActions} from 'vuex'
+  import anime from 'animejs/lib/anime.es.js'
+
+  import ProfilePosts from "@/components/profile/ProfilePosts";
+  import ProfileBanner from "@/components/profile/ProfileBanner";
+  import ProfileDescription from "@/components/profile/ProfileDescription";
+  import ProfileStatusChart from "@/components/profile/ProfileStatusChart";
+  import ProfileComments from "@/components/profile/ProfileComments";
+  import ProfileLikes from "@/components/profile/ProfileLikes";
+
+  export default {
+    data: () => {
+      return {
+        n: 10,
+        model: 0,
+        username: '',
+        selectedTab: 0,
+        comp: ProfilePosts,
+        componentObjects: [],
+        numberOfChildRendred: 0,
+        hash: ''
+      }
+    },
     components: {
-        ProfileIntro
+      ProfileStatusChart,
+      ProfileDescription,
+      ProfileBanner,
+      ProfilePosts
+    },
+    computed: {
+      ...mapGetters('modules/profile/profilePosts', ['getPosts']),
+      ...mapGetters('modules/profile/profileComments', ['getComments']),
+      ...mapGetters('modules/profile/profileLikes', ['getLikes']),
+      ...mapGetters('modules/profile/profileInfo', ['getProfile']),
+      getComponentObjects() {
+        if (this.comp == ProfilePosts) {
+          this.componentObjects = this.getPosts
+        } else if (this.comp == ProfileComments) {
+          this.componentObjects = this.getComments
+        } else {
+          this.componentObjects = this.getLikes
+        }
+        return this.componentObjects
+      }
+    },
+    created() {
+      this.username = this.$route.params.username;
+      console.log(this.$route);
+      this.getProfileInfo(this.username);
+      let query = this.$route.query.show;
+      this.switchToProperTab(query);
+      console.log(this.$route.params.username);
+      this.hash = this.$route.hash;
+    },
+    methods: {
+      ...mapActions('modules/profile/profileInfo', ['getProfileInfo']),
+      ...mapActions('modules/profile/profilePosts', ['getProfilePosts']),
+      ...mapActions('modules/profile/profileComments', ['getProfileComments']),
+      ...mapActions('modules/profile/profileLikes', ['getProfileLikes']),
+      switchToProperTab(query) {
+        if (query) {
+          if (query === 'comments') {
+            this.selectedTab = 1
+            this.switchToComments()
+            return
+          } else if (query === 'likes') {
+            this.selectedTab = 2
+            this.switchToLikes()
+            return
+          }
+        }
+        this.switchToPosts()
+      },
+      switchToPosts() {
+        this.getProfilePosts(this.username)
+          .catch((error) => {
+            this.showErrorWithMessage('Something went wrong')
+          })
+        this.comp = ProfilePosts
+      },
+      switchToComments() {
+        this.getProfileComments(this.username)
+          .catch((error) => {
+            this.showErrorWithMessage('Something went wrong')
+          })
+        this.comp = ProfileComments
+      },
+      switchToLikes() {
+        this.getProfileLikes(this.username)
+          .catch((error) => {
+            this.showErrorWithMessage('Something went wrong')
+          })
+        this.comp = ProfileLikes
+      },
+      componentArgs(index) {
+        if (this.comp == ProfilePosts) {
+          return {
+            post: this.getComponentObjectById(index)
+          }
+        } else if (this.comp == ProfileComments) {
+          return {
+            comment: this.getComponentObjectById(index)
+          }
+        } else {
+          return {
+            likedObj: this.getComponentObjectById(index),
+            nickname: this.getProfile.nickname
+          }
+        }
+      },
+      getComponentObjectById(id) {
+        return this.componentObjects[id]
+      },
+      setHash(hash) {
+        if (hash) {
+          let element_to_scroll_to = document.getElementById(hash.slice(1));
+          element_to_scroll_to.scrollIntoView();
+          setTimeout(() => {
+            anime({
+              targets: element_to_scroll_to,
+              opacity: 0.3,
+              direction: 'alternate',
+              duration: 1000,
+              easing: 'easeInOutSine'
+            });
+          }, 500)
+        }
+      },
+      countChild() {
+        this.numberOfChildRendred++
+        if (this.numberOfChildRendred == this.componentObjects.length) {
+          setTimeout(() => {
+            this.setHash(this.hash)
+          }, 1)
+        }
+      },
+      showErrorWithMessage(message) {
+        this.$notifier.showMessage({ content: message, color: 'error' });
+      }
     }
-}
+  }
 </script>
+
+<style scoped>
+
+
+  .tab-stick {
+    top: 0px;
+    position: sticky;
+    position: -webkit-sticky;
+    z-index: 5;
+  }
+</style>
