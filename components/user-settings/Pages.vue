99305b354d9a111a61053f3bbdd28f9b3b1e7cd2
Amir Esmaeili <amir-esmaeili@outlook.com>
2020-12-06 23:31:34 +0330

    profile editing page

---
+++ components/user-settings/Pages.vue
@@ -0,0 +1,47 @@
+<template>
+  <v-card
+    class="mx-auto"
+    max-width="300"
+    elevation="2"
+    tile>
+    <v-list rounded dense>
+      <v-subheader>Settings</v-subheader>
+      <v-list-item-group
+        :v-model="1"
+        color="primary">
+        <v-list-item
+          v-for="(item, i) in pages"
+          :to="item.link"
+          :key="i">
+          <v-list-item-icon>
+            <v-icon v-text="item.icon"></v-icon>
+          </v-list-item-icon>
+          <v-list-item-content>
+            <v-list-item-title v-text="item.label"></v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+      </v-list-item-group>
+    </v-list>
+  </v-card>
+</template>
+
+<script>
+export default {
+  name: "PagesList",
+  data() {
+    return {
+      pages: [
+        {icon: 'mdi-account', label: 'Profile', link: '/user-settings/profile'},
+        {icon: 'mdi-content-save', label: 'Drafts', link: '/user-settings/drafts'},
+        {icon: 'mdi-cog', label: 'Settings', link: '/user-settings/settings'},
+        {icon: 'mdi-account-group', label: 'My Communities', link: '/user-settings/communities'},
+        {icon: 'mdi-account-multiple', label: 'My Connections', link: '/user-settings/connections'},
+      ]
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ pages/user-settings.vue
@@ -0,0 +1,24 @@
+<template>
+  <v-container>
+    <v-row>
+      <v-col md="3">
+        <PagesList />
+      </v-col>
+      <v-col>
+        <NuxtChild />
+      </v-col>
+    </v-row>
+  </v-container>
+</template>
+
+<script>
+import PagesList from "~/components/user-settings/Pages";
+export default {
+  name: "user-settings",
+  components: {PagesList},
+}
+</script>
+
+<style scoped>
+
+</style>
