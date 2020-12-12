cdd36094ddb66bfd3c10dee03aa085875673de80
Amin-MAG <M.Amin.Ghasvari@gmail.com>
2020-12-13 03:03:18 +0330

    add follower following in feed page

--- components/homepage/User.vue
+++ components/homepage/User.vue
@@ -29,17 +29,17 @@
     <v-divider class="primary"/>
     <v-row class="text-center pa-2 justify-space-around">
       <div>
-        <b>{{ numberOfFollowings }}</b>
+        <b>{{ profileInfo.followings.length }}</b>
         <br>
         <span>Followings</span>
       </div>
       <div>
-        <b>{{ numberOfPosts }}</b>
+        <b>{{ profileInfo.posts.length }}</b>
         <br>
         <span>Posts</span>
       </div>
       <div>
-        <b>{{ numberOfFollowers }}</b>
+        <b>{{ profileInfo.followers.length }}</b>
         <br>
         <span>Followers</span>
       </div>
@@ -48,53 +48,78 @@
 </template>
 
 <script>
-import { mapActions } from 'vuex'
-import UserAvatar from "../shared/UserAvatar";
+  import {mapActions} from 'vuex'
+  import UserAvatar from "../shared/UserAvatar";
 
-export default {
-  name: "user.component",
-  components: { UserAvatar },
-  props: ['user'],
-  data: () => {
-    return {
-      numberOfPosts: 0,
-      numberOfFollowers: 0,
-      numberOfFollowings: 0,
+  export default {
+    name: "user.component",
+    components: { UserAvatar },
+    props: ['user'],
+    data: () => {
+      return {
+        profileInfo: {
+          posts: [],
+          followers: [],
+          followings: [],
+        }
+      }
+    },
+    mounted() {
+      this.fetchFollowers();
+      this.fetchFollowings();
+      this.fetchPosts();
+    },
+    methods: {
+      ...mapActions('modules/post', ['getPostByAuthor']),
+      ...mapActions('modules/follow', ['getFollowers']),
+      ...mapActions('modules/follow', ['getFollowings']),
+      fetchFollowings() {
+        this.getFollowings({
+          username: this.$auth.user.username
+        }).then(({ data }) => {
+          this.profileInfo.followings = data.user_followings;
+        }).catch((error) => {
+          this.$notifier.showMessage({ content: error.message, color: 'error' })
+        });
+      },
+      fetchFollowers() {
+        this.getFollowers({
+          username: this.$auth.user.username
+        }).then(({ data }) => {
+          this.profileInfo.followers = data.user_followers;
+        }).catch((error) => {
+          this.$notifier.showMessage({ content: error.message, color: 'error' })
+        });
+      },
+      fetchPosts() {
+        this.getPostByAuthor({
+          username: this.$auth.user.username
+        }).then(({ data }) => {
+          this.profileInfo.posts = data.all_user_posts;
+        }).catch((error) => {
+          this.$notifier.showMessage({ content: error.message, color: 'error' })
+        });
+      },
     }
-  },
-  mounted() {
-    this.getProfileInfo(this.user.username)
-    .then((profile) => {
-      this.numberOfPosts = profile.numberOfPosts;
-      this.numberOfFollowers = profile.numberOfFollowers;
-      this.numberOfFollowings = profile.numberOfFollowing;
-    })
-    .catch((error) => {
-
-    })
-  },
-  methods: {
-    ...mapActions('modules/profile/profileInfo', ['getProfileInfo'])
   }
-}
 </script>
 
 <style lang="scss" scoped>
-.profile {
-  position: relative;
+  .profile {
+    position: relative;
 
-  .profile-pic {
-    position: absolute;
-    bottom: -40px;
-    left: calc(50% - 56px);
+    .profile-pic {
+      position: absolute;
+      bottom: -40px;
+      left: calc(50% - 56px);
+    }
+
+    .custom-rounded {
+      border-radius: 5px 5px 0 0;
+    }
   }
 
-  .custom-rounded {
-    border-radius: 5px 5px 0 0;
+  b, span {
+    font-size: x-small;
   }
-}
-
-b, span {
-  font-size: x-small;
-}
 </style>
