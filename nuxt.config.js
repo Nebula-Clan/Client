1bf28920f157da4a8dbfe6092ec6e58966b70d7c
Amir Esmaeili <amir-esmaeili@outlook.com>
2020-12-14 13:35:56 +0330

    nuxt config

--- nuxt.config.js
+++ nuxt.config.js
@@ -59,7 +59,7 @@ export default {
 
   // Axios module configuration (https://go.nuxtjs.dev/config-axios)
   axios: {
-    baseURL: 'http://localhost:8000'
+    baseURL: 'http://huddle.pythonanywhere.com'
   },
 
   // Auth module configuration
