b0648ed01ffab59dd0f521a351f6626df22a26c7
keyvan1379 <ddssee71@gmail.com>
2020-12-16 04:36:03 -0800

    cube for loading added

--- pages/my-chats.vue
+++ pages/my-chats.vue
@@ -1,13 +1,16 @@
 <template>
     <v-app>
-        <v-container fluid class="pa-0">
-            <Chat />
+        <v-container fluid class="pa-0 fill-height">
+            <!-- <Chat /> -->
+            <LoadPage />
         </v-container>
     </v-app>
 </template>
 
 <script>
 import Chat from '~/components/chat/Chat'
+import LoadPage from '~/components/chat/LoadPage'
+
 export default {
     layout: 'chat',
     components: {
